729aec53cbdf07a8a9cc3c328514a326e73bf1be
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-03-23 02:43:39 +0500

    add 1-6 question

---
+++ smit assignment 7/app.js
@@ -0,0 +1,51 @@
+// Qno 1
+
+// var num = 3.45671
+
+// console.log(Math.round(num))
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+
+// Qno 2
+
+
+// var num = -3.45671
+
+// console.log(Math.round(num))
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+
+// Qno 3
+
+// var num = -4
+// var abs = Math.abs(num)
+
+// console.log("The absolute value of " + num + " is " + abs)
+
+// var num = 5
+// var abs = Math.abs(num)
+
+// console.log("The absolute value of " + num + " is " + abs)
+
+// Qno 4
+
+// var dice = Math.floor(Math.random() * 6) + 1;
+// console.log("The value of the dice is " + dice);
+
+
+// Qno 5
+
+// let coinToss = Math.floor(Math.random() * 2) === 0 ? "Heads" : "Tails";
+// console.log("The result of the coin toss is " + coinToss);
+
+// // Qno 6
+
+// var num = Math.floor(Math.random() * 100) + 1;
+
+// console.log(num)
+
+
+// Q no 7
+
